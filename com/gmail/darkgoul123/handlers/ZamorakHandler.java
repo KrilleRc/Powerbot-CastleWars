29c74a3f0017c27b22fb9f8e1c41ae76262d147c
Desmond Jackson <desmondjackson1@gmail.com>
2012-07-01 21:50:21 -0500

    Created the Zamorak Handler Class.

---
+++ com/gmail/darkgoul123/handlers/ZamorakHandler.java
@@ -0,0 +1,23 @@
+package com.gmail.darkgoul123.handlers;
+
+import org.powerbot.game.api.methods.interactive.Players;
+import org.powerbot.game.api.methods.node.SceneEntities;
+import org.powerbot.game.api.wrappers.Tile;
+import org.powerbot.game.api.wrappers.node.SceneObject;
+
+import com.gmail.darkgoul123.interfaces.EnvironmentImpl;
+import com.gmail.darkgoul123.interfaces.ZamorakImpl;
+
+public class ZamorakHandler implements ZamorakImpl, EnvironmentImpl {
+
+	public ZamorakHandler() {
+		Tile myLocation = Players.getLocal().getLocation();
+		SceneObject portal = SceneEntities.getNearest(zamorakPortalId);
+		if(castleArea.contains(myLocation)) {
+			GuthixHandler.enterPortal(portal);
+		} else {
+			new GuthixHandler();
+		}
+	}
+
+}
