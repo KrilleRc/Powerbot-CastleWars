883a3b4734457817e37ee8c00cd46af2904bd4a1
Desmond Jackson <desmondjackson1@gmail.com>
2012-07-01 21:49:10 -0500

    Created the main class.

---
+++ com/gmail/darkgoul123/CastleWars.java
@@ -0,0 +1,22 @@
+package com.gmail.darkgoul123;
+import org.powerbot.game.api.ActiveScript;
+import org.powerbot.game.api.Manifest;
+
+import com.gmail.darkgoul123.gui.TeamChooser;
+
+@Manifest(
+		authors = { "DarkGhoul45" },
+		name = "CastleWars",
+		version = 1.00D,
+		description = "Plays Castle Wars."
+)
+
+public class CastleWars extends ActiveScript {
+
+	@Override
+	protected void setup() {
+		new TeamChooser().setVisible(true);
+		submit(new Loop());
+	}
+
+}
